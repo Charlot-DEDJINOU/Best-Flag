7ebb135dd99d835a3013f5d7fe3203fb15b46de4
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-22 12:35:28 +0100

    app

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const apiRoutes = require('./routes/api');
+const cors = require('cors');
+const routemap = require('express-routemap');
+
+
+app.use(express.json());
+// app.use(cors)
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
+
+routemap(app);
+
+module.exports = app;
