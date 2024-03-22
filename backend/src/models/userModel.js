fde6ab6a16d00f530dfcd6ab9c1fc107c3388557
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-22 12:34:48 +0100

    user model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/sequelize');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  admin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = User;
