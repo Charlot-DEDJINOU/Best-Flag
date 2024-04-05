54ec1acfccb8cad9ef3770d4372d272911580e2a
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-05 16:54:33 +0100

    convention de nommage

--- cryptographie/main.py
+++ cryptographie/main.py
@@ -1,16 +1,16 @@
 import argparse
-import ROTN
-import Offset
-import AES
-import ASCII
-import Substitution
+import rotn
+import offset
+import aes
+import ascii
+import substitution
 
 def displayMethodError() :
     print('Error: Invalid method. Please provide a valid method (crypt or decrypt).')
 
 # Définition des arguments attendus depuis le terminal
 parser = argparse.ArgumentParser(description='Encrypt or decrypt a file using different methods.')
-parser.add_argument('-f', '--function', type=str, help='Name of the function to call (ROTN, Offset, AES, ASCII, Substitution)')
+parser.add_argument('-f', '--function', type=str, help='Name of the function to call (rotn, offset, aes, ascii, substitution)')
 parser.add_argument('-u', '--input', type=str, help='Input file name')
 parser.add_argument('-o', '--output', type=str, help='Output file name')
 parser.add_argument('-k', '--key', type=str, help='Key for encryption/decryption')
@@ -21,40 +21,40 @@ parser.add_argument('-m', '--method', type=str, help='Method to use (crypt or de
 args = parser.parse_args()
 
 # Vérification des arguments et appel des fonctions correspondantes
-if args.function == 'ROTN':
+if args.function == 'rotn':
     if args.method == 'crypt':
-        ROTN.crypt(args.input, args.output, int(args.key), args.alphabet)
+        rotn.crypt(args.input, args.output, int(args.key), args.alphabet)
     elif args.method == 'decrypt':
-        ROTN.decrypt(args.input, args.output, int(args.key), args.alphabet)
+        rotn.decrypt(args.input, args.output, int(args.key), args.alphabet)
     else:
         displayMethodError()
-elif args.function == 'Offset':
+elif args.function == 'offset':
     if args.method == 'crypt':
-        Offset.crypt(args.input, args.output, int(args.key))
+        offset.crypt(args.input, args.output, int(args.key))
     elif args.method == 'decrypt':
-        Offset.decrypt(args.input, args.output, int(args.key))
+        offset.decrypt(args.input, args.output, int(args.key))
     else:
         displayMethodError()
-elif args.function == 'AES':
+elif args.function == 'aes':
     if args.method == 'crypt':
-        AES.crypt(args.input, args.output, args.key.encode())
+        aes.crypt(args.input, args.output, args.key.encode())
     elif args.method == 'decrypt':
-        AES.decrypt(args.input, args.output, args.key.encode())
+        aes.decrypt(args.input, args.output, args.key.encode())
     else:
         displayMethodError()
-elif args.function == 'ASCII':
+elif args.function == 'ascii':
     if args.method == 'crypt':
-        ASCII.crypt(args.input, args.output, int(args.key))
+        ascii.crypt(args.input, args.output, int(args.key))
     elif args.method == 'decrypt':
-        ASCII.decrypt(args.input, args.output, int(args.key))
+        ascii.decrypt(args.input, args.output, int(args.key))
     else:
         displayMethodError()
-elif args.function == 'Substitution':
+elif args.function == 'substitution':
     if args.method == 'crypt':
-        Substitution.crypt(args.input, args.output, int(args.key))
+        substitution.crypt(args.input, args.output, int(args.key))
     elif args.method == 'decrypt':
-        Substitution.decrypt(args.input, args.output, int(args.key))
+        substitution.decrypt(args.input, args.output, int(args.key))
     else:
         displayMethodError()
 else:
-    print('Error: Invalid function name. Please provide a valid function name (ROTN, Offset, AES, ASCII, Substitution).')
+    print('Error: Invalid function name. Please provide a valid function name (rotn, offset, aes, ascii, substitution).')
