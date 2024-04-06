dae76fdac277808c03c264a0cd7a6526756c8f05
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-06 23:39:03 +0100

    steganographie d'image

--- steganographie/image_steganography.py
+++ steganographie/image_steganography.py
@@ -1,3 +1,4 @@
+from steganographie.file import read, write
 from PIL import Image
 
 def hide_image(image_to_hide_path, host_image_path, output_image_path):
@@ -35,9 +36,7 @@ def hide_image(image_to_hide_path, host_image_path, output_image_path):
             index += 1
         hidden_pixels.append((red, green, blue))
 
-    hidden_image = Image.new(host_image.mode, host_image.size)
-    hidden_image.putdata(hidden_pixels)
-    hidden_image.save(output_image_path)
+    create_image(host_image.size, host_image.mode, hidden_pixels, output_image_path)
 
     print(f"L'image cachée avec métadonnées binaires a été créée avec succès et enregistrée dans '{output_image_path}'.")
 
@@ -61,8 +60,6 @@ def extract_hidden_image(host_image_path, output_image_path):
     hidden_image_height = int(metadata[32:64],2)
     hidden_image_size = int(extracted_data[64:128], 2)
 
-    print(f"Taille de l'image cachée : {hidden_image_size} bits")
-
     hidden_image_data = extracted_data[128 : 128 + hidden_image_size]
 
     hidden_image_pixels = []
@@ -72,18 +69,14 @@ def extract_hidden_image(host_image_path, output_image_path):
         blue = int(hidden_image_data[i + 16 : i + 24], 2)
         hidden_image_pixels.append((red, green, blue))
 
-    hidden_image = Image.new('RGB', (hidden_image_width, hidden_image_height))
-    hidden_image.putdata(hidden_image_pixels)
-    hidden_image.save(output_image_path)
+    create_image((hidden_image_width, hidden_image_height), 'RGB', hidden_image_pixels, output_image_path)
+
     print(f"Image cachée extraite et enregistrée dans '{output_image_path}'.")
 
-from PIL import Image
-
-def encode_LSB(image_path, binary_data):
+def hide_text(image_path, binary_data):
     image = Image.open(image_path)
     pixels = list(image.getdata())
 
-    # Vérifier que la longueur des données binaires est inférieure au nombre total de pixels
     if len(binary_data) > len(pixels):
         raise ValueError("Les données binaires sont trop longues pour être cachées dans l'image.")
 
@@ -102,10 +95,61 @@ def encode_LSB(image_path, binary_data):
             index += 1
         encoded_pixels.append((red, green, blue))
 
-    encoded_image = Image.new(image.mode, image.size)
-    encoded_image.putdata(encoded_pixels)
-    encoded_image.save("encoded_image.png")
+    create_image(image.size, image.mode, encoded_pixels, "encoded_image.png")
 
-binary_data = '100110001111011101010100'
+def image_to_pixel(image_path, output_file):
+    
+    image = Image.open(image_path)
 
-encode_LSB("image_hote.png", binary_data)
+    pixels = list(image.getdata())
+
+    pixel_data = [f"({pixel[0]},{pixel[1]},{pixel[2]})" for pixel in pixels]
+
+    write(output_file, ';'.join(pixel_data))
+
+    print(f"Données des pixels enregistrées dans '{output_file}' avec succès.")
+
+
+def image_to_bits(image_path, output_file):
+   
+    image = Image.open(image_path)
+
+    binary_data = ''.join(format(pixel, '08b') for pixel in image.tobytes())
+
+    write(output_file, binary_data)
+
+    print(f"Données des pixels enregistrées dans '{output_file}' avec succès.")
+
+
+def image_from_bits(bits_file, output_image_path, image_size):
+
+    bits_data = read(bits_file)
+
+    pixels = []
+    for i in range(0, len(bits_data), 24):
+        red = int(bits_data[i:i+8], 2)
+        green = int(bits_data[i+8:i+16], 2)
+        blue = int(bits_data[i+16:i+24], 2)
+        pixels.append((red, green, blue))
+
+    create_image(image_size, 'RGB', pixels, output_image_path)
+
+    print("Image reconstituée et enregistrée avec succès.")
+
+def image_from_pixels(pixels_file, output_image_path, image_size):
+   
+    pixel_data = read(pixels_file)
+
+    pixels = []
+    for line in pixel_data.split(';'):
+        r, g, b = map(int, line.strip('()\n ').split(','))
+        pixels.append((r, g, b))
+
+    create_image(image_size, 'RGB', pixels, output_image_path)
+
+    print(f"Image créée et enregistrée dans '{output_image_path}' avec succès.")
+
+def create_image(size, mode, data, output_file) :
+    image = Image.new(mode, size)
+    image.putdata(data)
+    image.save(output_file)
