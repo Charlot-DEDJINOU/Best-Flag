c9a26bc86c994bc367b802a4ca0bdbc674db2a57
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-09 18:07:34 +0100

    Astuces data

---
+++ frontend/src/data/Astuces.js
@@ -0,0 +1,10 @@
+export default function Astuces() {
+    return [
+        "Le fichier best flag a été crypté avec 5 algorithmes successifs. Les scripts de cryptage et de déchiffrement de chaque algorithme sont à votre disposition.",
+        "Le fichier main.py permet d'interagir avec ces algorithmes depuis votre terminal. Prenez bien connaissance du Readme pour une utilisation optimale.",
+        "Pour exécuter ces algorithmes, il faut fournir les clés nécessaires pour un déchiffrement correct. Une mauvaise clé donnera un message mal déchiffré qui pourrait affecter le reste.",
+        "Les clés ont été dissimulées, c'est à vous de trouver chacune des clés et de déterminer à quel algorithme elles correspondent. Pour cela, veuillez suivre les étapes proposées.",
+        "Êtes-vous sûr que tous les scripts vous ont été donnés ?",
+        "Ce qui est évident est-il toujours vrai ?"
+    ]
+}
