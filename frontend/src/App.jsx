255d10912c296c3f8e1698bc19dee740d20f8991
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-09 18:07:16 +0100

    new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./views/Home"
+import Footer from "./components/Footer"
+import Substitution from "./views/Substitution"
 
 function App() {
 
@@ -9,7 +11,9 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={ <Home /> } />
+                <Route path="/substitution" element={ <Substitution /> } />
             </Routes>
+            <Footer />
         </Router>
     </React.StrictMode>
   )
