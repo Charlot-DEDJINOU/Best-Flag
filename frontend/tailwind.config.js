591bb6fd7d41bfb7ac100703d6ca31c9a236cf22
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-09 08:47:35 +0100

    config tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "1.5xl": "1408px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
