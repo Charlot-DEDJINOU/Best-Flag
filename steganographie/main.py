2b196e863a65b97eb3fd94ffeeb63c33f11d91e9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-09 08:46:56 +0100

    argparse

--- steganographie/main.py
+++ steganographie/main.py
@@ -5,8 +5,8 @@ def main():
     parser = argparse.ArgumentParser(description='Hide or extract data in/from images and perform text processing.')
     parser.add_argument('-a', '--action', type=str, choices=['hide_image', 'extract_hidden_image', 'hide_text', 'extract_text', 'image_to_pixel', 'image_to_bits', 'image_from_bits', 'image_from_pixels', 'save_binary_to_text', 'save_text_to_binary'], help='Action to perform')
     parser.add_argument('-t', '--text', type=str, help='Input file text path')
-    parser.add_argument('-h', '--image_to_image', type=str, help='Input file path (image to hide)')
-    parser.add_argument('-H', '--host_image', type=str, help='Input file path (host image)')
+    parser.add_argument('-sh', '--image_to_image', type=str, help='Input file path (image to hide)')
+    parser.add_argument('-th', '--host_image', type=str, help='Input file path (host image)')
     parser.add_argument('-o', '--output', type=str, help='Output file path')
     
     args = parser.parse_args()
