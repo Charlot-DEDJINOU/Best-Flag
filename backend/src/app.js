20d09154851518539bc70461b4fc9aaa0dac38d5
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-10 23:48:21 +0100

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); // Importez le package CORS
+
 const apiRoutes = require('./routes/api');
 const routemap = require('express-routemap');
 
 app.use(express.json());
-// app.use(cors)
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors({
+    origin: '*',
+    credentials: false
+}));
 
 app.use('/api', apiRoutes);
 
