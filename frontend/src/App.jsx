972c6def8febb92c8def485e466a128184e66a63
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-10 23:47:56 +0100

    new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./views/Home"
+import { UserProvider } from "./context/UserProvider"
 import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./views/Home"
 import Substitution from "./views/Substitution"
+import Ascii from "./views/Ascii"
 
 function App() {
 
   return (
     <React.StrictMode>
         <Router>
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-                <Route path="/substitution" element={ <Substitution /> } />
-            </Routes>
-            <Footer />
+            <UserProvider>
+              <Header />
+              <Routes>
+                  <Route path="/" element={ <Home /> } />
+                  <Route path="/substitution" element={ <Substitution /> } />
+                  <Route path="/ascii" element={ <Ascii /> } />
+              </Routes>
+              <Footer />
+            </UserProvider>
         </Router>
     </React.StrictMode>
   )
