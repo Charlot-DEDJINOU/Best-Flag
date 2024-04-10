73b73ed39d4897cc7e4b949fbfdcd680a16632df
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-10 23:45:50 +0100

    AlertDanger Component

---
+++ frontend/src/components/AlertDanger.jsx
@@ -0,0 +1,5 @@
+export default function AlertDanger({message}) {
+    return(
+        <div className="w-full px-4 py-2 rounded-md border font-bold border-[#A52A2A] bg-[#e6b7b7] text-[#A52A2A] mb-3">{message}</div>
+    )
+}
