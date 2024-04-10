efebe4f1fe0c834e737615c78f182ed3b0cce0bc
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-10 23:47:13 +0100

    add p-2

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,26 +1,24 @@
 import Button from "../components/Button"
 import Astuces from "../data/Astuces"
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+
+    const navigate = useNavigate()
+
     return(
-        <section id="home" className="w-full min-h-[73.8vh]">
+        <section id="home" className="w-full min-h-[47.5vh]">
            <div className="container m-auto flex flex-col items-center">
-                <div className="text-5xl text-[#A52A2A] my-5">BEST FLAG</div>
-                <div className="my-3 container">
-                    <p className="mb-3 text-center"> The Best Flag !!! Et si on s'entraînait un peu sur la cryptographie et la stéganographie ?</p>
-                    <p className="mb-5 text-center"> Parviendrez-vous à trouver le flag dans ce fichier <span className="text-[#A52A2A] hover:cursor-pointer">best_flag</span> ?
-                    Vous aurez sûrement besoin de ces <span className="text-[#A52A2A] hover:cursor-pointer">scripts Python</span> pour la cryptographie. Mais pour la stéganographie, c'est à vous de la découvrir.</p>
-                    <div className="mx-2">
-                        <p className="text-4xl text-[#A52A2A] mb-3">Astuces</p>
-                        {
-                            Astuces().map((item, index) => (
-                                <p key={index} className="mb-2"><span className=" text-[#A52A2A]">{ index + 1 }</span> - {item}</p>
-                            ))
-                        }
-                    </div>
+                <div className="mx-2">
+                    <p className="text-4xl text-[#A52A2A] mb-3">Astuces</p>
+                    {
+                        Astuces().map((item, index) => (
+                            <p key={index} className="mb-2"><span className=" text-[#A52A2A]">{ index + 1 }</span> - {item}</p>
+                        ))
+                    }
                 </div>
-                <Button>Commencer</Button>
-           </div>
+                <Button onClick={() => navigate('/substitution')}>Commencer</Button>
+            </div>
         </section>
     )
 }
