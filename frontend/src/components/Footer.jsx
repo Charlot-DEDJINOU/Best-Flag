ebca266c43f7cf11264bdd3cbe7a4f6eb09ada8f
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-10 23:47:38 +0100

    David Goggins as Actor

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="w-full bg-[#000000] p-5">
             <div className="container flex flex-col items-center">
                 <b className="text-center mb-3 text-3xl text-[#A52A2A]">BEST FLAG</b>
-                <p className="text-center"> L’être humain à le potentiel de devenir quelque chose d’autre - Albert Schweitzer</p>
+                <p className="text-center"> L’être humain à le potentiel de devenir quelque chose d’autre - David Goggins</p>
                 <div className="copyright my-4">
                     &copy; Copyright <strong><span>Best Flag </span></strong>All Rights Reserved 
                 </div>
