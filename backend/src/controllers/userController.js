f84d2fc5239f067ededc2c35dc235612b5c8350a
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-12 12:06:02 +0100

    news methods

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -73,12 +73,13 @@ exports.getInfos = async (req, res) => {
 
 exports.getMoreInfos = async (req, res) => {
   res.status(200).json({
-    "/login" : "Route pour la connexion",
-    "/aqwzsedcvfrtgbnhyujkiolmp" : "Route pour la creation de compte",
-    "/bjkloirtghdbchauiomsfhwad" : "Route pour la mise d'un utilisateur",
-    "Exemple Objet user" : {
-        username : "Charlot DEDJINOU",
-        password : "Nerys@apzoeiruty#2004@04#25"
-    }
-  })
-}
+      "/login": "Route pour la connexion",
+      [process.env.REGISTER_ROUTE]: "Route pour la création de compte",
+      [process.env.UPDATE_ROUTE]: "Route pour la mise à jour d'un utilisateur",
+      "Exemple Objet user": {
+          username: "Charlot DEDJINOU",
+          password: "Nerys@apzoeiruty#2004@04#25",
+          admin : false
+      }
+  });
+};
