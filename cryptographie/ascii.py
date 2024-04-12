9e59a8ec512c586acc169b2208f107c0076685d0
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-12 12:02:24 +0100

    function getKey

--- cryptographie/ascii.py
+++ cryptographie/ascii.py
@@ -1,8 +1,10 @@
 from file import read, write
+from math import floor
 
 def crypt(input_file, output_file, key) :
     messages = read(input_file).split(chr(10))
     content = list()
+    key = getKey(key)
     for message in messages :
         shiffer = ' '.join([str(ord(c) + key) for c in message])
         content.append(shiffer)
@@ -12,8 +14,18 @@ def crypt(input_file, output_file, key) :
 def decrypt(input_file, output_file, key) :
     messages = read(input_file).split(chr(10))
     content = list()
+    key = getKey(key)
     for message in messages:
         deshiffer = ''.join([chr(int(c) - key) for c in message.split()])
         content.append(deshiffer)
 
     write(output_file, chr(10).join(content))
+
+def getKey(s) :
+    num = 0
+    dem = 0
+    for (index, item) in enumerate(s) :
+        num += ord(item) + index
+        dem += index * 2
+
+    return floor(num/dem) + 1
