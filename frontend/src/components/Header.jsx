7dc066cc22fa696f53ab4e8bd6af78d59700fdc6
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-12 12:06:20 +0100

    components header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header>
             <div className="container m-auto flex flex-col items-center">
                 <div className="text-5xl text-[#A52A2A] my-5">BEST FLAG</div>
-                <div className="container my-3">
+                <div className="container my-3 px-3">
                     <p className="mb-3 text-center"> The Best Flag !!! Et si on s'entraînait un peu sur la cryptographie et la stéganographie ?</p>
                     <p className="mb-5 text-center"> Parviendrez-vous à trouver le flag dans ce fichier <span className="text-[#A52A2A] hover:cursor-pointer">best_flag</span> ?
                     Vous aurez sûrement besoin de ces <span className="text-[#A52A2A] hover:cursor-pointer">scripts Python</span> pour la cryptographie. Mais pour la stéganographie, c'est à vous de la découvrir.</p>
