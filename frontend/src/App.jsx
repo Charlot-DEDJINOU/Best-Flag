3d6bd85f6384b03504e42bca291bd0a2fc8baa0e
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-12 12:06:49 +0100

    route /aes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header"
 import Home from "./views/Home"
 import Substitution from "./views/Substitution"
 import Ascii from "./views/Ascii"
+import Aes from "./views/Aes"
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
                   <Route path="/" element={ <Home /> } />
                   <Route path="/substitution" element={ <Substitution /> } />
                   <Route path="/ascii" element={ <Ascii /> } />
+                  <Route path="/aes" element={ <Aes /> } />
               </Routes>
               <Footer />
             </UserProvider>
