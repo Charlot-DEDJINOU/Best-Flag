2201de9d65a8ceaaca3b6b75acea61a5cafebf16
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-12 12:00:12 +0100

    new routes

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -4,12 +4,13 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getInfos)
-router.post('/aqwzsedcvfrtgbnhyujkiolmp', userController.register);
+router.post(process.env.REGISTER_ROUTE, userController.register);
 router.post('/login', userController.login);
 
 router.use(authMiddleware);
 
+router.get('/ascii', userController.getAsciiKey)
 router.get('/update', userController.getMoreInfos);
-router.put('/bjkloirtghdbchauiomsfhwad', userController.update);
+router.put(process.env.UPDATE_ROUTE, userController.update);
 
 module.exports = router;
