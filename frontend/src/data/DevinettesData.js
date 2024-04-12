bd6541e7a9a527023a9455927128ca54ac4667f9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-12 13:38:48 +0100

    Devenettes Data

---
+++ frontend/src/data/DevinettesData.js
@@ -0,0 +1,8 @@
+export default function DevinettesData() {
+    return [
+        "Je suis au milieu de la mer et au bout du monde , Qui suis je ?",
+        "Qu'est-ce qui a un cou mais pas de tête ?",
+        "Je suis toujours devant tout le monde mais personne ne peut me voir. Qui suis-je",
+        "Tout le monde en a une, mais personne ne peut la perdre , Qui suis je ?"
+    ]
+}
