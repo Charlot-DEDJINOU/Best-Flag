dca4a817659e0fa8129dd9d73c22b334dafa8af6
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-18 13:14:26 +0100

    loading Button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,5 +1,16 @@
-export default function Button({onClick, className, children}) {
+import ReloadIcon from './icons/ReloadIcon'
+
+export default function Button({onClick, className, isLoading = false, children}) {
     return(
-        <button className={`bg-[#A52A2A] py-2 px-3 rounded-md my-3 ${className}`} onClick={onClick}>{ children }</button>
+        <button className={`bg-[#A52A2A] py-2 px-3 rounded-md my-3 flex flex-row items-center ${className}`} onClick={onClick}>
+            {
+                isLoading ? (
+                    <>
+                        <ReloadIcon className="animate-spin"/>
+                        <span className="block mx-2">Loading...</span>
+                    </>
+                ) : children
+            }
+        </button>
     )
 }
